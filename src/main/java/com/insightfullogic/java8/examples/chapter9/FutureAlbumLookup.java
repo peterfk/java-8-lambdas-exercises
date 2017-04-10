3fed54697215a364c9ca178efda5e4b30345ab0c
Peter Kipping <pkipping@yahoo.com>
2017-04-09 17:22:00 -0700

    formatting

--- src/main/java/com/insightfullogic/java8/examples/chapter9/FutureAlbumLookup.java
+++ src/main/java/com/insightfullogic/java8/examples/chapter9/FutureAlbumLookup.java
@@ -12,60 +12,59 @@ import java.util.concurrent.Future;
 
 public class FutureAlbumLookup implements AlbumLookup {
 
-    private static final ExecutorService service = Executors.newFixedThreadPool(2);
+	private static final ExecutorService service = Executors.newFixedThreadPool(2);
 
-    private final List<Track> tracks;
-    private final List<Artist> artists;
+	private final List<Track> tracks;
+	private final List<Artist> artists;
 
-    public FutureAlbumLookup(List<Track> tracks, List<Artist> artists) {
-        this.tracks = tracks;
-        this.artists = artists;
-    }
+	public FutureAlbumLookup(List<Track> tracks, List<Artist> artists) {
+		this.tracks = tracks;
+		this.artists = artists;
+	}
 
-    // BEGIN lookupByName
-@Override
-public Album lookupByName(String albumName) {
-    Future<Credentials> trackLogin = loginTo("track"); // <1>
-    Future<Credentials> artistLogin = loginTo("artist");
+	// BEGIN lookupByName
+	@Override
+	public Album lookupByName(String albumName) {
+		Future<Credentials> trackLogin = loginTo("track"); // <1>
+		Future<Credentials> artistLogin = loginTo("artist");
 
-    try {
-        Future<List<Track>> tracks = lookupTracks(albumName, trackLogin.get()); // <2>
-        Future<List<Artist>> artists = lookupArtists(albumName, artistLogin.get());
+		try {
+			Future<List<Track>> tracks = lookupTracks(albumName, trackLogin.get()); // <2>
+			Future<List<Artist>> artists = lookupArtists(albumName, artistLogin.get());
 
-        return new Album(albumName, tracks.get(), artists.get()); // <3>
-    } catch (InterruptedException | ExecutionException e) {
-        throw new AlbumLookupException(e.getCause()); // <4>
-    }
-}
-    // END lookupByName
+			return new Album(albumName, tracks.get(), artists.get()); // <3>
+		} catch (InterruptedException | ExecutionException e) {
+			throw new AlbumLookupException(e.getCause()); // <4>
+		}
+	}
+	// END lookupByName
 
-    // ----------------- FAKE LOOKUP METHODS -----------------
-    //         Represent API lookup on external services
+	// ----------------- FAKE LOOKUP METHODS -----------------
+	//         Represent API lookup on external services
+	private Future<List<Artist>> lookupArtists(String albumName, Credentials credentials) {
+		return service.submit(() -> {
+			fakeWaitingForExternalWebService();
+			return artists;
+		});
+	}
 
-    private Future<List<Artist>> lookupArtists(String albumName, Credentials credentials) {
-        return service.submit(() -> {
-            fakeWaitingForExternalWebService();
-            return artists;
-        });
-    }
+	private Future<List<Track>> lookupTracks(String albumName, Credentials credentials) {
+		return service.submit(() -> {
+			return tracks;
+		});
+	}
 
-    private Future<List<Track>> lookupTracks(String albumName, Credentials credentials) {
-        return service.submit(() -> {
-            return tracks;
-        });
-    }
+	private Future<Credentials> loginTo(String serviceName) {
+		return service.submit(() -> {
+			if ("track".equals(serviceName)) {
+				fakeWaitingForExternalWebService();
+			}
+			return new Credentials();
+		});
+	}
 
-    private Future<Credentials> loginTo(String serviceName) {
-        return service.submit(() -> {
-            if ("track".equals(serviceName)) {
-                fakeWaitingForExternalWebService();
-            }
-            return new Credentials();
-        });
-    }
-
-    private void fakeWaitingForExternalWebService() throws InterruptedException {
-        Thread.sleep(1000);
-    }
+	private void fakeWaitingForExternalWebService() throws InterruptedException {
+		Thread.sleep(1000);
+	}
 
 }
